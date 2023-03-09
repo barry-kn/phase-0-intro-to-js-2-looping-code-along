6362362d679301648a55d3e7c6e7e7936deb822c
barry-kn <carrybarryklein49@gmail.com>
2023-03-09 21:24:53 +0300

    7th commite

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
+
+
 function writeCards(names,eventName){
-  let messages = [];
+    let messages = [];
     for(let i = 0;i<names.length; i++ ) {
-      console.log(i) ;
-      console.log(messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`));
+        console.log(messages) ;
+        console.log((`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`));
     }
     return messages ;
-    }
-  
-writeCards(["Guadalupe", "Ollie", "Aki"], "birthday" ) ;
+     }
+     writeCards(["Guadalupe", "Ollie", "Aki"], "birthday" ) ;
+
+ 
 
 
 
 function countDown(num){
-       while(num>=0){
-         console.log(num);
-           num-- ;}
+        while(num>=0){
+           console.log(num);
+         num-- ;}
         }        
 countDown(10);
