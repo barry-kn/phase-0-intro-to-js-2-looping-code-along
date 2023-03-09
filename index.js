3e4406f89e6d50a7cc0b13de0b8d124b991d0273
barry-kn <carrybarryklein49@gmail.com>
2023-03-09 21:03:24 +0300

    6th commit

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
-functiriteCards(names,eventName){
-    let messages = [];
+function writeCards(names,eventName){
+  let messages = [];
     for(let i = 0;i<names.length; i++ ) {
-        console.log(i) ;
-        console.log(messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`));
-    {
+      console.log(i) ;
+      console.log(messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`));
+    }
     return messages ;
-     }
-
+    }
+  
 writeCards(["Guadalupe", "Ollie", "Aki"], "birthday" ) ;
 
 
 
-
 function countDown(num){
-        while(num>=0){
-           console.log(num);
-           num-- ;
-        }
+       while(num>=0){
+         console.log(num);
+           num-- ;}
         }        
 countDown(10);
